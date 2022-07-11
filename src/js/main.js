bc31c96751cf41160df79119e5fb8251a98e45f1
unjusodal <81766638+unjusodal@users.noreply.github.com>
2022-07-11 23:25:15 +0300

    Styled footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
+const body = document.querySelector('body')
 const menuBtn = document.querySelector('.menu__button')
 const nav = document.querySelector('.nav__menu')
 
 menuBtn.addEventListener('click', () => {
     nav.classList.toggle('nav__menu--active')
+    body.classList.toggle('disable-scroll')
 
     document.querySelector('.header__content').addEventListener('click', () => {
         nav.classList.remove('nav__menu--active')
+        body.classList.remove('disable-scroll')
     })
 })
